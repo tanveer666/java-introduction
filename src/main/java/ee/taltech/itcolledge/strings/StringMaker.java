da7a668ce5baebbda8415edd44c94dfccd703da9
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-01-09 13:03:51 +0200

    Java-collections-2 examples

---
+++ src/main/java/ee/taltech/itcolledge/strings/StringMaker.java
@@ -0,0 +1,32 @@
+package ee.taltech.itcolledge.strings;
+
+public class StringMaker {
+
+    /**
+     * Gives you a text containing of x times of character c.
+     * @param c the character to repeat
+     * @param x how many times you want the character
+     */
+    public static String xCharacters(char c, int x) {
+        String chars = "";
+        return chars;
+    }
+
+    /**
+     * Gives you a text containing of x times of character c plus y times of character c.
+     */
+    public static String xCharacters(char c1, int x1, char c2, int x2) {
+        String chars = "";
+        return chars;
+    }
+
+    public static String xString(String s, int x) {
+        String chars = "";
+        return chars;
+    }
+
+    public static String xString(String s1, int x1, String s2, int x2) {
+        String chars = "";
+        return chars;
+    }
+}
