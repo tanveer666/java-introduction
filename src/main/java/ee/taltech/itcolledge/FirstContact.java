db69f7938863925a7685ac501db24fa1468bcc7c
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-02-11 09:36:05 +0200

    Update master

--- src/main/java/ee/taltech/itcolledge/FirstContact.java
+++ src/main/java/ee/taltech/itcolledge/FirstContact.java
@@ -4,5 +4,7 @@ public class FirstContact {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Everybody loves java");
+        System.out.println("So should you");
     }
 }
