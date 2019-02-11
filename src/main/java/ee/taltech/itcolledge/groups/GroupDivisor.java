d735378825b0f3d588f65ff4c59678e7c05e26db
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-02-11 21:08:28 +0200

    Add solution to math3

--- src/main/java/ee/taltech/itcolledge/groups/GroupDivisor.java
+++ src/main/java/ee/taltech/itcolledge/groups/GroupDivisor.java
@@ -5,27 +5,20 @@ import java.util.Scanner;
 public class GroupDivisor {
 
     public static void main(String[] args) {
-        System.out.println("Enter your name: ");
         Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        System.out.println("You said your name is: " + line);
 
-        // todo using example above create a program to calculate number of groups
         System.out.println("---------=============---------");
-        System.out.println("\n");
-        System.out.println("\n");
         System.out.println("Insert the number of people");
-        int people = 0;
+        int people = sc.nextInt();
         System.out.println("Insert the group size");
-        int size = 0;
+        int size = sc.nextInt();
 
-        //todo create tests for your calculator
-        int groups = 0;
-        int leftover = 0;
+        int groups = people / size;
+        int leftover = people % size;
+        int leftover2 = people - groups * size;
 
         System.out.println(String.format("You get %d full groups, leftover %d.", groups, leftover));
+        System.out.println(String.format("You get %d full groups, leftover %d.", groups, leftover2));
         System.out.println("---------=============---------");
-        System.out.println("\n");
-        System.out.println("\n");
     }
 }
--- src/main/java/ee/taltech/itcolledge/groups/ProfitCalculator.java
+++ src/main/java/ee/taltech/itcolledge/groups/ProfitCalculator.java
@@ -1,14 +1,19 @@
 package ee.taltech.itcolledge.groups;
 
+import java.util.Scanner;
+
 public class ProfitCalculator {
 
     public static void main(String[] args) {
-        // todo using example in group divisor create a program to calculate return on investment (ROI)
-        System.out.println("Insert the initial investment");
-        int investment = 0;
-        System.out.println("Insert the expected return a year %");
-        int percentage = 0;
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(String.format("You get %d money back, out of which %d. is pure profit", 0, 0));
+        System.out.println("Insert the initial investment");
+        int initialInvestment = Integer.parseInt(sc.nextLine());
+        System.out.println("Insert the expected return a year %");
+        int percentage = Integer.parseInt(sc.nextLine());
+
+        int returnOnInvestment = initialInvestment * percentage / 100;
+        int total = initialInvestment + returnOnInvestment;
+        System.out.println(String.format("You get %d money back, out of which %d is pure profit", total, returnOnInvestment));
     }
 }
